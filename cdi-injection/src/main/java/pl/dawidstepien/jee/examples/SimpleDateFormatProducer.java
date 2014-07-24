78cb050afbd289324fea5323f39fef59d8dd13c7
dstepien <kontakt@dawidstepien.pl>
2014-07-24 16:21:36 +0200

    Add example of a disposer method

--- cdi-injection/src/main/java/pl/dawidstepien/jee/examples/SimpleDateFormatProducer.java
+++ cdi-injection/src/main/java/pl/dawidstepien/jee/examples/SimpleDateFormatProducer.java
@@ -1,11 +1,16 @@
 package pl.dawidstepien.jee.examples;
 
 import java.text.SimpleDateFormat;
+import java.util.logging.Logger;
 
+import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
 public class SimpleDateFormatProducer {
+
+  @Inject
+  Logger logger;
 
   @Inject @SimpleDatePattern
   private String pattern;
@@ -13,5 +18,9 @@ public class SimpleDateFormatProducer {
   @Produces
   public SimpleDateFormat produceSimpleDateFormat() {
     return new SimpleDateFormat(pattern);
+  }
+
+  private void objectCreated(@Disposes SimpleDateFormat simpleDateFormat) {
+    logger.info("SimpleDateFormat object created.");
   }
 }
